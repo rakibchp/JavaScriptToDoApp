e747f520b86dd2ede08e4141651a64c18547037d
Abdur Rakib <rakibchp@gmail.com>
2022-12-28 00:00:05 +0600

    .js file updated

--- App.js
+++ App.js
@@ -16,4 +16,68 @@ const showMessage = (text, status) => {
     }, 1000);
 }
 
-//
+// creteTodo 
+const creteTodo = (newTodo) => {
+    const todoElement = document.createElement("li");
+    todoElement.id = newTodo.todoId;
+    todoElement.classList.add("li-style");
+    todoElement.innerHTML = `
+    <span> ${newTodo.todoValue} <span/>
+    <span> <button class ="btn" id="deleteButton"> <i class="fa fa-trash"> </> </button><span/>
+    `;
+    todoLists.appendChild(todoElement);
+
+    const deleteButton = todoElement.querySelector("#deleteButton");
+    deleteButton.addEventListener("click", deleteTodo);
+};
+
+//deleteTodo
+const deleteTodo = (event) => {
+    const selectedTodo = event.target.parentElement.parentElement.parentElement;
+
+    todoLists.removeChild(selectedTodo);
+    showMessage("Todo is deleted", "danger");
+
+    let todos = getTodosFromLocalStorage();
+    todos = todos.filter((todo) => todo.todoId !== selectedTodo.id);
+    localStorage.setItem("mytodos", JSON.stringify(todos));
+};
+
+//getTodosFromLocalStorage
+const getTodosFromLocalStorage = () => {
+    return localStorage.getItem("mytodos")
+    ? JSON.parse(localStorage.getItem("mytodos"))
+    : [];
+};
+
+// addTodo 
+const addTodo = (event) => {
+    event.preventDefault();
+    const todoValue = todoInput.ariaValueMax;
+
+    //unique id
+    const todoId = Date.now().toString();
+
+    const newTodo = new Todo(todoId, todoValue);
+
+    creteTodo(newTodo);
+    showMessage("Todo is added", "success");
+
+    // add todo to locatStorage
+    const todos = getTodosFromLocalStorage();
+    todos.push(newTodo);
+    localStorage.setItem("mytodos", JSON.stringify(todos));
+
+    todoInput.value = "";
+};
+
+
+// loadTodos
+const loadTodos = () => {
+    const todos = getTodosFromLocalStorage();
+    todos.map((todo) => creteTodo(todo));
+};
+
+// addidng listeners 
+todoForm.addEventListener("submit", addTodo);
+window.addEventListener("DOMContentLoaded", loadTodos);
